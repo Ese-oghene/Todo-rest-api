d3868e0774083f980ee9a55c979af6579c950188
Ese-oghene <oghenee08@gmail.com>
2025-03-19 21:53:30 +0100

    add scibe

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -34,12 +34,5 @@ class TaskRepository implements TaskRepositoryInterface
     }
 }
 
-interface TaskRepositoryInterface
-{
-    public function getAllTasks();
-    public function getTaskById($id);
-    public function createTask(array $data);
-    public function updateTask(Task $task, array $data);
-    public function deleteTask(Task $task);
-}
+
 
